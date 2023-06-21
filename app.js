2c053cd63b8e06b7868dfe0d41177c9121b5ac49
arnav <arnav04jan@gmail.com>
2023-06-21 23:03:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+function fetchHackerNews() {
+    return fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+        .then(response => response.json())
+        .then(data => {
+            const topStories = data.slice(0, 10); // Fetching only the top 10 stories
+            const storyPromises = topStories.map(storyId =>
+                fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+                    .then(response => response.json())
+            );
+            return Promise.all(storyPromises);
+        });
+}
+
+function displayHackerNews() {
+    const contentSection = document.querySelector('.content');
+    const blogPostsContainer = document.createElement('div');
+    blogPostsContainer.classList.add('blog-posts');
+    contentSection.appendChild(blogPostsContainer);
+
+    fetchHackerNews().then(stories => {
+        stories.forEach(story => {
+            const blogPostElement = document.createElement('div');
+            blogPostElement.classList.add('blog-post');
+            blogPostElement.innerHTML = `
+          <h2 class="blog-title"><a href="${story.url}">Visit>> </a>${story.title}</h2>
+        `;
+            blogPostsContainer.appendChild(blogPostElement);
+        });
+    });
+}
+
+
+displayHackerNews();
